0014c16ffde57c5969431cd238180ee70b90d15d
Felipe Palacios <felipe.palacios13@inacapmail.cl>
2024-10-23 15:22:17 -0300

    actualización para mostrar web

--- websiteDjango/smartGourmetProject/gourmetApp/views.py
+++ websiteDjango/smartGourmetProject/gourmetApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexData(request):
-    return render(request, 'empleadosApp/index.html')
+    return render(request, 'gourmetApp/index.html')
