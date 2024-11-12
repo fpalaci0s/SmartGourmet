cacf8fb51c6c8bdcde4605290b0980fd3b6bf1de
Felipe Palacios <felipe.palacios13@inacapmail.cl>
2024-11-12 12:29:04 -0300

    algunas correcciones y intentando añadir paginas

--- websiteDjango/smartGourmetProject/gourmetApp/views.py
+++ websiteDjango/smartGourmetProject/gourmetApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def indexData(request):
     return render(request, 'gourmetApp/index.html')
+
+def contactos(request):
+    return render(request, 'gourmetApp/contacts.html')
